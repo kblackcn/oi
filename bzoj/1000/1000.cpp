fd16cdbe64b8b4c9c5a50906e08123dffb3bba5d
KBlack <zhouyiranhen2@outlook.com>
2016-04-03 21:14:05 +0800

    + bzoj.1000

---
+++ bzoj/1000/1000.cpp
@@ -0,0 +1,17 @@
+/**************************************************************
+    Problem: 1000
+    User: syswow64
+    Language: C++
+    Result: Accepted
+    Time:0 ms
+    Memory:804 kb
+****************************************************************/
+ 
+#include<cstdio>
+ 
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a+b);
+    return 0;
+}
